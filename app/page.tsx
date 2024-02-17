943019063744475d3171d8c9aaee9e27acfed7a8
Derick <derickchoskinson@gmail.com>
2024-02-17 01:40:58 -0600

    nothing exciting

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,41 @@
-import ImageCarousel from '@/components/image-carousel/image-carousel';
 import { getAllImages } from './actions';
+import React from 'react'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from
+  '@/components/ui/card'
+  import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+import { ArrowBigLeft } from 'lucide-react';
+import ImageCarousel from '@/components/image-carousel/image-carousel';
 
-async function Home () {
+async function Home ({ searchParams }: {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  }
+}) {
+
+  const pages = typeof searchParams.page === 'string' ? parseInt(searchParams.page) : 1;
+  const limit = typeof searchParams.limit === 'string' ? parseInt(searchParams.limit) : 10;
+
 
   const images = await getAllImages({
-    page: 1,
-    limit: 10,
+    page: pages,
+    limit: limit
 
   });
+  if (images.length === 0) {
+    return <div>No images found</div>;
+  }
 
 
 
   return (
-        <div
-        className="flex relative justify-center min-w-full">
-      <ImageCarousel images={ images }
+
+    <>
+      <ImageCarousel
+        imageArray={images}
 
       />
-          </div>
+    </>
 
   );
 }
