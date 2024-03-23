257299989c8e276ba2efe474b52e897fed44167a
Derick <derickchoskinson@gmail.com>
2024-03-23 00:45:42 -0500

    save

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,11 @@
 // prettier.config.js
 module.exports = {
+    tabwidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    trailingComma: 'none',
+    jsxSingleQuote:true,
     tailwindConfig: './tailwind.config.ts',
     tailwindFunctions: [ 'clsx', 'cn' ],
     plugins: [ 'prettier-plugin-tailwindcss' ],
