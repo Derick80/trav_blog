22e35da8af0dc18f11cbb86b0a93b507fe73fc63
Derick <derickchoskinson@gmail.com>
2024-03-28 03:28:05 -0500

    installed mdx

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default async function AboutPage ({ }: {}) {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Some information about the app</p>
+        </div>
+    )
+}
